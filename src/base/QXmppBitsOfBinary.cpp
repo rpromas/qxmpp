a4e4869850fcce154649fb2857959ca5adc10697
Linus Jahn <lnj@kaidan.im>
2025-06-05 04:10:55 +0200

    Merge BitsOfBinary translation units

--- src/base/QXmppBitsOfBinary.cpp
+++ src/base/QXmppBitsOfBinary.cpp
@@ -3,11 +3,270 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
 #include "QXmppBitsOfBinaryContentId.h"
+#include "QXmppBitsOfBinaryDataList.h"
+#include "QXmppBitsOfBinaryIq.h"
+#include "QXmppConstants_p.h"
+#include "QXmppUtils_p.h"
 
 #include "StringLiterals.h"
+#include "XmlWriter.h"
 
-#include <QMap>
+#include <QDomElement>
+#include <QMimeDatabase>
 #include <QSharedData>
+
+using namespace QXmpp::Private;
+
+class QXmppBitsOfBinaryDataPrivate : public QSharedData
+{
+public:
+    QXmppBitsOfBinaryDataPrivate();
+
+    QXmppBitsOfBinaryContentId cid;
+    int maxAge;
+    QMimeType contentType;
+    QByteArray data;
+};
+
+QXmppBitsOfBinaryDataPrivate::QXmppBitsOfBinaryDataPrivate()
+    : maxAge(-1)
+{
+}
+
+///
+/// \class QXmppBitsOfBinaryData
+///
+/// QXmppBitsOfBinaryData represents a data element for \xep{0231, Bits of
+/// Binary}. It can be used as an extension in other stanzas.
+///
+/// \see QXmppBitsOfBinaryIq, QXmppBitsOfBinaryDataList
+///
+/// \since QXmpp 1.2
+///
+
+///
+/// Creates bits of binary data from a QByteArray.
+///
+/// This hashes the data to generate a content ID. The MIME type is not set.
+///
+/// \note This blocks while hashing the data. You may want to run this via QtConcurrent or a
+/// QThreadPool to run this on for large amounts of data.
+///
+/// \since QXmpp 1.5
+///
+QXmppBitsOfBinaryData QXmppBitsOfBinaryData::fromByteArray(QByteArray data)
+{
+    QXmppBitsOfBinaryContentId cid;
+    cid.setHash(QCryptographicHash::hash(data, QCryptographicHash::Sha1));
+    cid.setAlgorithm(QCryptographicHash::Sha1);
+
+    QXmppBitsOfBinaryData bobData;
+    bobData.d->cid = std::move(cid);
+    bobData.d->data = std::move(data);
+
+    return bobData;
+}
+
+///
+/// Default constructor
+///
+QXmppBitsOfBinaryData::QXmppBitsOfBinaryData()
+    : d(new QXmppBitsOfBinaryDataPrivate)
+{
+}
+
+/// Default copy-constructor
+QXmppBitsOfBinaryData::QXmppBitsOfBinaryData(const QXmppBitsOfBinaryData &) = default;
+/// Default move-constructor
+QXmppBitsOfBinaryData::QXmppBitsOfBinaryData(QXmppBitsOfBinaryData &&) = default;
+/// Default destructor
+QXmppBitsOfBinaryData::~QXmppBitsOfBinaryData() = default;
+/// Default assignment operator
+QXmppBitsOfBinaryData &QXmppBitsOfBinaryData::operator=(const QXmppBitsOfBinaryData &) = default;
+/// Default move-assignment operator
+QXmppBitsOfBinaryData &QXmppBitsOfBinaryData::operator=(QXmppBitsOfBinaryData &&) = default;
+
+///
+/// Returns the content id of the data
+///
+QXmppBitsOfBinaryContentId QXmppBitsOfBinaryData::cid() const
+{
+    return d->cid;
+}
+
+///
+/// Sets the content id of the data
+///
+void QXmppBitsOfBinaryData::setCid(const QXmppBitsOfBinaryContentId &cid)
+{
+    d->cid = cid;
+}
+
+///
+/// Returns the time in seconds the data should be cached
+///
+/// A value of 0 means that the data should not be cached, while a value of -1
+/// means that nothing was set.
+///
+/// The default value is -1.
+///
+int QXmppBitsOfBinaryData::maxAge() const
+{
+    return d->maxAge;
+}
+
+///
+/// Sets the time in seconds the data should be cached
+///
+/// A value of 0 means that the data should not be cached, while a value of -1
+/// means that nothing was set.
+///
+/// The default value is -1.
+///
+void QXmppBitsOfBinaryData::setMaxAge(int maxAge)
+{
+    d->maxAge = maxAge;
+}
+
+///
+/// Returns the content type of the data
+///
+/// \note This is the advertised content type and may differ from the actual
+/// content type of the data.
+///
+QMimeType QXmppBitsOfBinaryData::contentType() const
+{
+    return d->contentType;
+}
+
+///
+/// Sets the content type of the data
+///
+void QXmppBitsOfBinaryData::setContentType(const QMimeType &contentType)
+{
+    d->contentType = contentType;
+}
+
+///
+/// Returns the included data in binary form
+///
+QByteArray QXmppBitsOfBinaryData::data() const
+{
+    return d->data;
+}
+
+///
+/// Sets the data in binary form
+///
+void QXmppBitsOfBinaryData::setData(const QByteArray &data)
+{
+    d->data = data;
+}
+
+///
+/// Returns true, if \c element is a \xep{0231, Bits of Binary} data element
+///
+bool QXmppBitsOfBinaryData::isBitsOfBinaryData(const QDomElement &element)
+{
+    return element.tagName() == u"data" && element.namespaceURI() == ns_bob;
+}
+
+/// \cond
+void QXmppBitsOfBinaryData::parseElementFromChild(const QDomElement &dataElement)
+{
+    d->cid = QXmppBitsOfBinaryContentId::fromContentId(dataElement.attribute(u"cid"_s));
+    d->maxAge = dataElement.attribute(u"max-age"_s, u"-1"_s).toInt();
+    d->contentType = QMimeDatabase().mimeTypeForName(dataElement.attribute(u"type"_s));
+    d->data = QByteArray::fromBase64(dataElement.text().toUtf8());
+}
+
+void QXmppBitsOfBinaryData::toXmlElementFromChild(QXmlStreamWriter *writer) const
+{
+    auto maxAge = d->maxAge > -1 ? std::make_optional(d->maxAge) : std::nullopt;
+
+    XmlWriter(writer).write(Element {
+        XmlTag,
+        OptionalAttribute { u"cid", d->cid.toContentId() },
+        OptionalAttribute { u"max-age", maxAge },
+        OptionalAttribute { u"type", d->contentType.name() },
+        Characters { Base64 { d->data } },
+    });
+}
+/// \endcond
+
+///
+/// Returns true, if cid, maxAge, contentType and data equal.
+///
+bool QXmppBitsOfBinaryData::operator==(const QXmppBitsOfBinaryData &other) const
+{
+    return d->cid == other.cid() &&
+        d->maxAge == other.maxAge() &&
+        d->contentType == other.contentType() &&
+        d->data == other.data();
+}
+
+///
+/// \class QXmppBitsOfBinaryDataList
+///
+/// QXmppBitsOfBinaryDataList represents a list of data elements from
+/// \xep{0231, Bits of Binary}.
+///
+/// \since QXmpp 1.2
+///
+
+QXmppBitsOfBinaryDataList::QXmppBitsOfBinaryDataList() = default;
+
+QXmppBitsOfBinaryDataList::~QXmppBitsOfBinaryDataList() = default;
+
+/// \cond
+void QXmppBitsOfBinaryDataList::parse(const QDomElement &element)
+{
+    // clear previous data elements
+    clear();
+
+    // parse all <data/> elements
+    for (const auto &dataEl : iterChildElements<QXmppBitsOfBinaryData>(element)) {
+        QXmppBitsOfBinaryData data;
+        data.parseElementFromChild(dataEl);
+        append(std::move(data));
+    }
+}
+
+void QXmppBitsOfBinaryDataList::toXml(QXmlStreamWriter *writer) const
+{
+    for (const auto &bitsOfBinaryData : *this) {
+        bitsOfBinaryData.toXmlElementFromChild(writer);
+    }
+}
+/// \endcond
+
+///
+/// \class QXmppBitsOfBinaryIq
+///
+/// QXmppBitsOfBinaryIq represents a \xep{0231, Bits of Binary} IQ to request
+/// and transmit Bits of Binary data elements.
+///
+/// \since QXmpp 1.2
+///
+
+QXmppBitsOfBinaryIq::QXmppBitsOfBinaryIq() = default;
+
+QXmppBitsOfBinaryIq::~QXmppBitsOfBinaryIq() = default;
+
+/// \cond
+void QXmppBitsOfBinaryIq::parseElementFromChild(const QDomElement &element)
+{
+    auto child = firstChildElement(element, u"data", ns_bob);
+    if (!child.isNull()) {
+        QXmppBitsOfBinaryData::parseElementFromChild(child);
+    }
+}
+
+void QXmppBitsOfBinaryIq::toXmlElementFromChild(QXmlStreamWriter *writer) const
+{
+    QXmppBitsOfBinaryData::toXmlElementFromChild(writer);
+}
+/// \endcond
 
 #define CONTENTID_URL u"cid:"_s
 #define CONTENTID_URL_LENGTH 4
--- tests/qxmppbitsofbinary/tst_qxmppbitsofbinary.cpp
+++ tests/qxmppbitsofbinary/tst_qxmppbitsofbinary.cpp
@@ -10,19 +10,39 @@
 #include <QMimeType>
 #include <QObject>
 
-class tst_QXmppBitsOfBinaryIq : public QObject
+Q_DECLARE_METATYPE(QCryptographicHash::Algorithm)
+
+class tst_QXmppBitsOfBinary : public QObject
 {
     Q_OBJECT
 
 private:
-    Q_SLOT void testBasic();
+    Q_SLOT void testIq();
     Q_SLOT void testResult();
     Q_SLOT void testOtherSubelement();
     Q_SLOT void testIsBobIq();
     Q_SLOT void fromByteArray();
+
+    Q_SLOT void testContentId();
+
+    Q_SLOT void testFromContentId_data();
+    Q_SLOT void testFromContentId();
+
+    Q_SLOT void testFromCidUrl_data();
+    Q_SLOT void testFromCidUrl();
+
+    Q_SLOT void testEmpty();
+
+    Q_SLOT void testIsValid_data();
+    Q_SLOT void testIsValid();
+
+    Q_SLOT void testIsBobContentId_data();
+    Q_SLOT void testIsBobContentId();
+
+    Q_SLOT void testUnsupportedAlgorithm();
 };
 
-void tst_QXmppBitsOfBinaryIq::testBasic()
+void tst_QXmppBitsOfBinary::testIq()
 {
     const QByteArray xml(
         "<iq id=\"get-data-1\" "
@@ -53,7 +73,7 @@ void tst_QXmppBitsOfBinaryIq::testBasic()
     serializePacket(iq, xml);
 }
 
-void tst_QXmppBitsOfBinaryIq::testResult()
+void tst_QXmppBitsOfBinary::testResult()
 {
     const QByteArray xml = QByteArrayLiteral(
         "<iq id=\"data-result\" "
@@ -133,7 +153,7 @@ void tst_QXmppBitsOfBinaryIq::testResult()
     serializePacket(iq, xml);
 }
 
-void tst_QXmppBitsOfBinaryIq::testOtherSubelement()
+void tst_QXmppBitsOfBinary::testOtherSubelement()
 {
     const QByteArray xml(
         "<iq id=\"get-data-1\" "
@@ -156,7 +176,7 @@ void tst_QXmppBitsOfBinaryIq::testOtherSubelement()
     QCOMPARE(iq.maxAge(), -1);
 }
 
-void tst_QXmppBitsOfBinaryIq::testIsBobIq()
+void tst_QXmppBitsOfBinary::testIsBobIq()
 {
     QT_WARNING_PUSH
     QT_WARNING_DISABLE_DEPRECATED
@@ -191,7 +211,7 @@ void tst_QXmppBitsOfBinaryIq::testIsBobIq()
     QT_WARNING_POP
 }
 
-void tst_QXmppBitsOfBinaryIq::fromByteArray()
+void tst_QXmppBitsOfBinary::fromByteArray()
 {
     auto data = QByteArray::fromBase64(
         "iVBORw0KGgoAAAANSUhEUgAAALQAAAA8BAMAAAA9AI20AAAAG1BMVEX///8AAADf39+"
@@ -220,5 +240,166 @@ void tst_QXmppBitsOfBinaryIq::fromByteArray()
     QCOMPARE(bobData.data().size(), size);
 }
 
-QTEST_MAIN(tst_QXmppBitsOfBinaryIq)
-#include "tst_qxmppbitsofbinaryiq.moc"
+void tst_QXmppBitsOfBinary::testContentId()
+{
+    // test fromCidUrl()
+    QXmppBitsOfBinaryContentId cid = QXmppBitsOfBinaryContentId::fromCidUrl(QStringLiteral(
+        "cid:sha1+8f35fef110ffc5df08d579a50083ff9308fb6242@bob.xmpp.org"));
+
+    QCOMPARE(cid.algorithm(), QCryptographicHash::Sha1);
+    QCOMPARE(cid.hash().toHex(), QByteArrayLiteral("8f35fef110ffc5df08d579a50083ff9308fb6242"));
+    QCOMPARE(cid.toCidUrl(), u"cid:sha1+8f35fef110ffc5df08d579a50083ff9308fb6242@bob.xmpp.org"_s);
+    QCOMPARE(cid.toContentId(), u"sha1+8f35fef110ffc5df08d579a50083ff9308fb6242@bob.xmpp.org"_s);
+
+    // test fromContentId()
+    cid = QXmppBitsOfBinaryContentId::fromContentId(QStringLiteral(
+        "sha1+8f35fef110ffc5df08d579a50083ff9308fb6242@bob.xmpp.org"));
+
+    QCOMPARE(cid.algorithm(), QCryptographicHash::Sha1);
+    QCOMPARE(cid.hash().toHex(), QByteArrayLiteral("8f35fef110ffc5df08d579a50083ff9308fb6242"));
+    QCOMPARE(cid.toCidUrl(), u"cid:sha1+8f35fef110ffc5df08d579a50083ff9308fb6242@bob.xmpp.org"_s);
+    QCOMPARE(cid.toContentId(), u"sha1+8f35fef110ffc5df08d579a50083ff9308fb6242@bob.xmpp.org"_s);
+
+    // test setters
+    cid = QXmppBitsOfBinaryContentId();
+    cid.setHash(QByteArray::fromHex(QByteArrayLiteral("8f35fef110ffc5df08d579a50083ff9308fb6242")));
+    cid.setAlgorithm(QCryptographicHash::Sha1);
+
+    QCOMPARE(cid.algorithm(), QCryptographicHash::Sha1);
+    QCOMPARE(cid.hash().toHex(), QByteArrayLiteral("8f35fef110ffc5df08d579a50083ff9308fb6242"));
+    QCOMPARE(cid.toCidUrl(), u"cid:sha1+8f35fef110ffc5df08d579a50083ff9308fb6242@bob.xmpp.org"_s);
+    QCOMPARE(cid.toContentId(), u"sha1+8f35fef110ffc5df08d579a50083ff9308fb6242@bob.xmpp.org"_s);
+}
+
+void tst_QXmppBitsOfBinary::testFromContentId_data()
+{
+    QTest::addColumn<QString>("input");
+    QTest::addColumn<bool>("isValid");
+
+#define ROW(NAME, INPUT, IS_VALID) \
+    QTest::newRow(NAME) << QStringLiteral(INPUT) << IS_VALID
+
+    ROW("valid", "sha1+8f35fef110ffc5df08d579a50083ff9308fb6242@bob.xmpp.org", true);
+    ROW("wrong-namespace", "sha1+8f35fef110ffc5df08d579a50083ff9308fb6242@bob_222.xmpp.org", false);
+    ROW("no-namespace", "sha1+8f35fef110ffc5df08d579a50083ff9308fb6242@", false);
+    ROW("url", "cid:sha1+8f35fef110ffc5df08d579a50083ff9308fb6242@bob.xmpp.org", false);
+    ROW("url-and-wrong-namespace", "cid:sha1+8f35fef110ffc5df08d579a50083ff9308fb6242@bob_222.xmpp.org", false);
+    ROW("too-many-pluses", "sha1+sha256+sha3-256+blake2b256+8f35fef110ffc5df08d579a50083ff9308fb6242@bob.xmpp.org", false);
+    ROW("wrong-hash-length", "cid:sha1+08d579a50083ff9308fb6242@bob.xmpp.org", false);
+
+#undef ROW
+}
+
+void tst_QXmppBitsOfBinary::testFromContentId()
+{
+    QFETCH(QString, input);
+    QFETCH(bool, isValid);
+
+    QCOMPARE(QXmppBitsOfBinaryContentId::fromContentId(input).isValid(), isValid);
+}
+
+void tst_QXmppBitsOfBinary::testFromCidUrl_data()
+{
+    QTest::addColumn<QString>("input");
+    QTest::addColumn<bool>("isValid");
+
+#define ROW(NAME, INPUT, IS_VALID) \
+    QTest::newRow(NAME) << QStringLiteral(INPUT) << IS_VALID
+
+    ROW("valid", "cid:sha1+8f35fef110ffc5df08d579a50083ff9308fb6242@bob.xmpp.org", true);
+    ROW("no-url", "sha1+8f35fef110ffc5df08d579a50083ff9308fb6242@bob.xmpp.org", false);
+    ROW("wrong-namespace", "cid:sha1+8f35fef110ffc5df08d579a50083ff9308fb6242@other", false);
+    ROW("too-many-pluses", "cid:sha1+sha256+sha3-256+blake2b256+8f35fef110ffc5df08d579a50083ff9308fb6242@bob.xmpp.org", false);
+#undef ROW
+}
+
+void tst_QXmppBitsOfBinary::testFromCidUrl()
+{
+    QFETCH(QString, input);
+    QFETCH(bool, isValid);
+
+    QCOMPARE(QXmppBitsOfBinaryContentId::fromCidUrl(input).isValid(), isValid);
+}
+
+void tst_QXmppBitsOfBinary::testEmpty()
+{
+    QXmppBitsOfBinaryContentId cid;
+    QVERIFY(cid.toCidUrl().isEmpty());
+    QVERIFY(cid.toContentId().isEmpty());
+}
+
+void tst_QXmppBitsOfBinary::testIsValid_data()
+{
+    QTest::addColumn<QByteArray>("hash");
+    QTest::addColumn<QCryptographicHash::Algorithm>("algorithm");
+    QTest::addColumn<bool>("isValid");
+
+#define ROW(NAME, HASH, ALGORITHM, IS_VALID) \
+    QTest::newRow(NAME) << QByteArray::fromHex(HASH) << ALGORITHM << IS_VALID
+
+    ROW("valid",
+        "8f35fef110ffc5df08d579a50083ff9308fb6242",
+        QCryptographicHash::Sha1,
+        true);
+    ROW("valid-sha256",
+        "01ba4719c80b6fe911b091a7c05124b64eeece964e09c058ef8f9805daca546b",
+        QCryptographicHash::Sha256,
+        true);
+    ROW("wrong-hash-length", "8f35fef110ffc5df08", QCryptographicHash::Sha1, false);
+
+#undef ROW
+}
+
+void tst_QXmppBitsOfBinary::testIsValid()
+{
+    QFETCH(QByteArray, hash);
+    QFETCH(QCryptographicHash::Algorithm, algorithm);
+    QFETCH(bool, isValid);
+
+    QXmppBitsOfBinaryContentId contentId;
+    contentId.setAlgorithm(algorithm);
+    contentId.setHash(hash);
+
+    QCOMPARE(contentId.isValid(), isValid);
+}
+
+void tst_QXmppBitsOfBinary::testIsBobContentId_data()
+{
+    QTest::addColumn<QString>("input");
+    QTest::addColumn<bool>("checkIsUrl");
+    QTest::addColumn<bool>("isValid");
+
+#define ROW(NAME, INPUT, CHECK_IS_URL, IS_VALID) \
+    QTest::newRow(NAME) << QStringLiteral(INPUT) << CHECK_IS_URL << IS_VALID
+
+    ROW("valid-url-check-url", "cid:sha1+8f35fef110ffc5df08d579a50083ff9308fb6242@bob.xmpp.org", true, true);
+    ROW("valid-url-no-check-url", "cid:sha1+8f35fef110ffc5df08d579a50083ff9308fb6242@bob.xmpp.org", false, true);
+    ROW("valid-id-no-check-url", "sha1+8f35fef110ffc5df08d579a50083ff9308fb6242@bob.xmpp.org", false, true);
+    ROW("not-an-url", "sha1+8f35fef110ffc5df08d579a50083ff9308fb6242@bob.xmpp.org", true, false);
+
+    ROW("invalid-namespace-id", "sha1+8f35fef110ffc5df08d579a50083ff9308fb6242@bob.xmpp.org.org.org", false, false);
+    ROW("invalid-namespace-url", "cid:sha1+8f35fef110ffc5df08d579a50083ff9308fb6242@bob.xmpp.org.org.org", true, false);
+
+    ROW("no-hash-algorithm", "sha18f35fef110ffc5df08d579a50083ff9308fb6242@bob.xmpp.org", false, false);
+#undef ROW
+}
+
+void tst_QXmppBitsOfBinary::testIsBobContentId()
+{
+    QFETCH(QString, input);
+    QFETCH(bool, checkIsUrl);
+    QFETCH(bool, isValid);
+
+    QCOMPARE(QXmppBitsOfBinaryContentId::isBitsOfBinaryContentId(input, checkIsUrl), isValid);
+}
+
+void tst_QXmppBitsOfBinary::testUnsupportedAlgorithm()
+{
+    QCOMPARE(
+        QXmppBitsOfBinaryContentId::fromContentId(
+            u"blake2s160+8f35fef110ffc5df08d579a50083ff9308fb6242@bob.xmpp.org"_s),
+        QXmppBitsOfBinaryContentId());
+}
+
+QTEST_MAIN(tst_QXmppBitsOfBinary)
+#include "tst_qxmppbitsofbinary.moc"
